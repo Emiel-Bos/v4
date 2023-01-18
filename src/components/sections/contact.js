e92b620bddcae6dfd021633a639343735f1ae2bf
Emiel Bos <ebos450@live.nl>
2023-01-18 11:39:17 +0000

    update contact.js section

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -60,8 +60,10 @@ const Contact = () => {
       <h2 className="title">Get In Touch</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        Currently looking for any new opportunities. Please feel free to contact me if you have a
+        question about my work or just want to say hi.
+        <br />
+        My inbox is always open and I'll try my best to get back to you!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
